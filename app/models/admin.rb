a69d9c42a0c2420c557f7d1822769fc35f13930f
Andrey <im@andreystarkov>
2017-03-23 18:13:59 +0500

    Using polices, Pundit

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,4 @@
 class Admin < ActiveRecord::Base
-  devise :database_authenticatable, :trackable, :timeoutable, :lockable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :trackable, :timeoutable, :lockable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   validates :email, presence: true
   validates :password, length: 6..20
 end
